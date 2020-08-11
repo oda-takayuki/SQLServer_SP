5465f40eeeb0a24ef48dff14fb4dca0c964a76cb
oda <oda.takayuki@m.alhinc.jp>
2020-08-12 01:18:43 +0900

    commit

--- Level2/trigger_history.sql
+++ Level2/trigger_history.sql
@@ -3,8 +3,9 @@ ON test2
 AFTER INSERT
 AS
 BEGIN	
-	DECLARE @create_date DATETIME = GETDATE(),
-			@serial_number DECIMAL(5,0) = (SELECT MAX(serial_number)+1 from history)
+	DECLARE 
+		@create_date DATETIME = GETDATE(),
+		@serial_number DECIMAL(5,0) = (SELECT MAX(serial_number)+1 from history)
 	INSERT INTO history(
 		serial_number,
 		user_id,
@@ -12,11 +13,12 @@ BEGIN
 		proc_type,
 		create_date
 	)
-	SELECT	@serial_number,
-			user_id,
-			acct_id,
-			1 AS proc_type,
-			getdate()
+	SELECT	
+		@serial_number,
+		user_id,
+		acct_id,
+		1 AS proc_type,
+		@create_date
 	FROM inserted;
 END
 GO
@@ -25,8 +27,9 @@ ON test2
 AFTER UPDATE
 AS
 BEGIN
-	DECLARE @create_date DATETIME = GETDATE(),
-			@serial_number DECIMAL(5,0) = (SELECT MAX(serial_number)+1 from history)
+	DECLARE
+		@create_date DATETIME = GETDATE(),
+		@serial_number DECIMAL(5,0) = (SELECT MAX(serial_number)+1 from history)
 	INSERT INTO history(
 		serial_number,
 		user_id,
@@ -34,10 +37,11 @@ BEGIN
 		proc_type,
 		create_date 
 	)
-	SELECT  @serial_number,
-			user_id,
-			acct_id,
-			2 AS proc_type,
-			@create_date
+	SELECT 
+		@serial_number,
+		user_id,
+		acct_id,
+		2 AS proc_type,
+		@create_date
 	FROM inserted;
 END
